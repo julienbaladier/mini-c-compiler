d04c0438e9ec50519b2cf3c711f0999f187889e9
Julien Baladier <julien.baladier@gmail.com>
2015-03-18 12:02:05 +0100

    Modification structure Instructions_stack

--- Instructions_stack/Instructions_stack.c
+++ Instructions_stack/Instructions_stack.c
@@ -13,7 +13,7 @@ int cmp_instructions(list_node * node, void * instruction){
 void print_instruction(void * data){
 	printf("Instruction number : %d | Position : %ld | ", 
 		((Instruction *)data)->i_instruction_number, ((Instruction *)data)->l_position);
-	if (((Instruction *)data)->b_written){
+	if (((Instruction *)data)->b_incomplete){
 		printf("Written");
 	}else{
 		printf("Not written");
@@ -33,11 +33,11 @@ void print_instructions_stack(llist instructions_stack){
 }
 
 /* Méthode qui permet d'empiler un élément */
-Instruction  *push_instruction(llist * instructions_stack, int i_instruction_number, long l_position, bool b_written){
+Instruction  *push_instruction(llist * instructions_stack, int i_instruction_number, long l_position, bool b_incomplete){
 	Instruction *instruction = (Instruction *) malloc(sizeof(Instruction));
 	instruction->i_instruction_number = i_instruction_number;
 	instruction->l_position = l_position;
-	instruction->b_written = b_written;
+	instruction->b_incomplete = b_incomplete;
 	list_node *node = list_insert_beginning(instructions_stack, instruction);
 	return (node ? (Instruction*)node->data : NULL);	 
 }
