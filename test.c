49a386c604bc81f309cf6ff919712099479460ee
Julien Baladier <julien.baladier@gmail.com>
2015-06-01 11:15:44 +0200

    Test calcul des 5 premiers élements de la suite de fibo

--- test.c
+++ test.c
@@ -1,9 +1,20 @@
-int salut(int a, const int b){
-	a = 43;
-}
-
-
+int fibonacci(int n){
+   if (n == 0){
+      return 0;
+   }else if (n == 1){
+      return 1;
+   }else{
+      return (fibonacci(n-1) + fibonacci(n-2));
+   }
+} 
+ 
 int main(){
-	int a, b = 45;
-	salut(a, b);
+   int i = 0, s;
+   while(i < 5){
+      s = fibonacci(i);
+      printf(s);
+      i = i + 1;
+   }
+   return 0;
 }
+ 
